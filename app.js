02ce72c6f5388e33944f182ef8d32314c539a6d6
Joshua McKinney <mckinneyj@byui.edu>
2018-01-22 16:23:44 -0700

    split error up

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error =  err;
+  res.locals.status =  err.status;
+  res.locals.stack =  err.stack;
   //res.locals.error = req.app.get('env') === 'development' ? err : {};
   
   // render the error page
